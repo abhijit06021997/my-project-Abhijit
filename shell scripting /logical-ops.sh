3c0df7569dec660292463fb579570abb8bb15efc
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-16 02:11:31 +0000

    Create logical-ops.sh

---
+++ shell scripting /logical-ops.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+
+read -p "what is your age?" age        #set of iput for user according to user input it will output 
+read -p "your country?" country
+
+if [[ $age -ge 18 ]] && [[ $country == "india" ]]
+
+then
+       echo "you can vote"
+else
+        echo "you cant vote in india"
+fi
