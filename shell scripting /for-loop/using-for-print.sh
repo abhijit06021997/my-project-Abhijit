a03270934fc0806a4b954f70f5bba282ffdfe9a6
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-21 17:33:09 +0000

    Create using-for-print.sh

---
+++ shell scripting /for-loop/using-for-print.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#for getting values from file
+
+FILE="/home/abhijit-ajit/folder/file1.txt"
+
+for name in $(cat $FILE)
+
+do
+        echo "name is $name"
+
+done
+
+#create 1 folder and file in that directory to execute those data using script 
+~                                                                                
