a5704bc5a1c07f9bf5f0034730eb23710b201d52
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-22 01:17:24 +0000

    Create while-loop.sh

---
+++ shell scripting /while-loop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+count=0
+num=10
+while [ $count -le $num ]
+do
+        echo "numbers are $count"
+
+let count++
+
+        done
+
+
+#while loop is used to condition will be true ub=ntil and unless values full that we sepcifiles e.g 10 from 0 it will incree from 0 till 10
