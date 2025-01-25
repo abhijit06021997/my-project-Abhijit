63f6ee167991484048e9cc55bc9871532569e57c
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-25 16:17:05 +0000

    Create function_with_argu1.sh

---
+++ shell scripting /functions/function_with_argu1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#to make funtion simple way 1
+function myfunction3 {
+echo "welcome  execute it $1"   #simple arguemt for 1st access we can acess with argument it print with our name 
+
+ }
+#1 function call two people 1 function will execute 1 for abhijit 1 for ajit 
+myfunction3 abhijit
+myfunction3 ajit
