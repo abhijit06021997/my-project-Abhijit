1a421ed8aacc3984fb29125f01cee210b9226065
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-25 15:52:35 +0000

    Create function_way1.sh

---
+++ shell scripting /functions/function_way1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#to make funtion simple way 1
+function myfunction1 {
+echo "welcome abhijit execute it many time for one function"
+
+ }
+
+#for call our function 
+
+myfunction1
+myfunction1
+myfunction1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# block of code which perform some task & run when it is called.
+-can be reuse many times u=in our programme which lession our lines of code .
+-we can pass arguments to the method .
