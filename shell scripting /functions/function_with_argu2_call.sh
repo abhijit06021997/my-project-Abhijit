f13894bda119d10eddd879e97ed488fe3875e10c
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-25 16:26:56 +0000

    Create function_with_argu2_call.sh

---
+++ shell scripting /functions/function_with_argu2_call.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#to make funtion simple way 1
+function myfunction3 {
+echo "welcome  execute it $1"   #call for first variable 
+echo "your age is $2"  # call  for 2nd variable 
+
+ }
+#1 function call two people 1 function will execute 1 for abhijit 1 for ajit 
+myfunction3 abhijit 27    #here are two argument pass for ist and 2 (first will show first funtion for one person and 2nd function will show for 2nd person 
+myfunction3 ajit 25    
+
+# argument used to call with defined values thre we specify $1 for one function and function will print for specied argument $2 function will print for specifuied 2nd argument
