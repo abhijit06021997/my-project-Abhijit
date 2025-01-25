0175d7ae813a5bf6d3342bf31d5d6924d5feb204
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-25 15:55:32 +0000

    Create function_way2.sh

---
+++ shell scripting /functions/function_way2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#to make funtion simple way 1
+myfunctionway2(){
+echo "welcome abhijit execute it many time for one function"
+
+ }
+#for call our function 
+myfunctionway2
+myfunctionway2
+myfunctionway2
