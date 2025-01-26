159c9d5b3682e88b3e4294278d66c9d7a1e48cf7
Abhijit Ramteke <134359078+abhijit06021997@users.noreply.github.com>
2025-01-26 15:02:27 +0000

    Create one_ormany_argu.sh

---
+++ shell scripting /arguments/one_ormany_argu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "hello value one $1"
+echo "value for tw $2"
+
+echo "value for all -$@"
+echo "how many number values - $#"
+
+
+# passw value while executing this scripting for e.g this file name is file.sh 
+# execute it ---file.sh abhijit ajit sonu monu 
+
+#first function will first argument 2nd function for e=2nd argument $@ --denioted for many argument wjile $# --denoted for number of argument 
